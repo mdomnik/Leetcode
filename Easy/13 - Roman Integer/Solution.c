a11643be5162341b725837714e3936e80c48c5a8
mdomnik <mdomnik.maciej@gmail.com>
2023-12-17 20:33:37 +0100

    Completed Roman To integer

--- Easy/13 - Roman Integer/Solution.c
+++ Easy/13 - Roman Integer/Solution.c
@@ -1,55 +1,56 @@
 #include <stdio.h>
 
+struct romanNums 
+{
+    char    Roman;
+    int     Num;
+};
+
+void populate(struct romanNums *Nbrs)
+{
+    Nbrs[0] = (struct romanNums) {'I', 1};
+    Nbrs[1] = (struct romanNums) {'V', 5};
+    Nbrs[2] = (struct romanNums) {'X', 10};
+    Nbrs[3] = (struct romanNums) {'L', 50};
+    Nbrs[4] = (struct romanNums) {'C', 100};
+    Nbrs[5] = (struct romanNums) {'D', 500};
+    Nbrs[6] = (struct romanNums) {'M', 1000};
+}
+
+int getValue(struct romanNums *Nbrs, char c) {
+    for (int i = 0; i < 7; i++) {
+        if (Nbrs[i].Roman == c) {
+            return Nbrs[i].Num;
+        }
+    }
+    return 0;
+}
+
 int romanToInt(char* s) 
 {
     int result = 0;
-    int i = 0;
-    while (s[i])
+    int prev = 0;
+    struct romanNums romanValues[7];
+    populate(romanValues);
+    while (*s != '\0')
     {
-        if (s[i] == 'C' && (s[i + 1] == 'M' || s[i + 1] == 'D'))
-            result -= 100;
-        else if (s[i] == 'x' && (s[i + 1] == 'L' || s[i + 1] == 'C'))
-            result -= 10;
-        else if (s[i] == 'I' && (s[i + 1] == 'V' || s[i + 1] == 'X'))
-            result -= 1;
-        else if (s[i] == 'I')
-            result += 1;
-        else if (s[i] == 'V')
-            result += 5;
-        else if (s[i] == 'X')
-            result += 10; 
-        else if (s[i] == 'L')
-            result += 50;
-        else if (s[i] == 'C')
-            result += 100;
-        else if (s[i] == 'D')
-            result += 500;
-        else if (s[i] == 'M')
-            result += 1000;
-        i++;
+        int current = getValue(romanValues, *s);
+        if (current > prev) 
+            result += current - 2 * prev;
+        else 
+            result += current;
+        prev = current;
+        s++;
     }
     return (result);
 }
 
-int main (void)
-{
-    char *s = "III";
-    printf("%d", s);
+int main() {
+    char romanNum[] = "CMIV"; // Roman numeral to test (14)
+    int result = romanToInt(romanNum);
+    
+    printf("Roman Numeral: %s\n", romanNum);
+    printf("Integer Value: %d\n", result);
+    
+    return 0;
 }
-
-// Symbol       Value
-// I             1
-// V             5
-// X             10
-// L             50
-// C             100
-// D             500
-// M             1000
-// For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
-
-// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
-
-// I can be placed before V (5) and X (10) to make 4 and 9. 
-// X can be placed before L (50) and C (100) to make 40 and 90. 
-// C can be placed before D (500) and M (1000) to make 400 and 900.
-// Given a roman numeral, convert it to an integer.
